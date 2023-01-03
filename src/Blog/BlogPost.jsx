ae28fd52f2178aae2096d07a4b2597d052abfd74
popTheSmart <anthony1.greco@ryerson.ca>
2023-01-03 00:13:03 -0500

    fix styling for blog post

--- src/Blog/BlogPost.jsx
+++ src/Blog/BlogPost.jsx
@@ -8,29 +8,19 @@ function BlogPost() {
   let userTitleUrl = params.titleUrl;
 
   // Fetch post from posts.json
-  let fetchedPost = {};
-  postJson.forEach((post, i) => {
-    if (userTitleUrl == post.titleUrl) {
-      fetchedPost.titleString = post.titleString
-        ? post.titleString
-        : "No title given";
-      fetchedPost.date = post.date ? post.date : "No date given";
-      fetchedPost.author = post.author ? post.author : "No author given";
-      fetchedPost.content = post.content ? post.content : "No content given";
-    }
-  });
+  let fetchedPost = postJson.find(
+    (element) => element.titleUrl == userTitleUrl
+  );
 
   return (
-    <div>
+    <article className="mx-7 mb-7 w-auto lg:mx-auto lg:w-[675px]">
       <h1 className="mb-2 text-4xl">{fetchedPost.titleString}</h1>
-      <p className="mb-12">
-        Published on {fetchedPost.date} {/*by {fetchedPost.author}*/}
-      </p>
+      <p className="mb-12">Posted on {fetchedPost.date}</p>
       <ReactMarkdown
         children={fetchedPost.content}
         components={markdownComponents}
       />
-    </div>
+    </article>
   );
 }
 
