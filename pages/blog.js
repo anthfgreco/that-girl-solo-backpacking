9c89b422d02f120ac4b5969c95161cdc1e1f402b
popTheSmart <anthony1.greco@ryerson.ca>
2023-02-05 18:11:15 -0500

    add fully styled blog main page

--- pages/blog.js
+++ pages/blog.js
@@ -1,13 +1,56 @@
 import BlankLayout from "@/components/blankLayout";
 import Head from "next/head";
+import { getSortedPostsData } from "@/lib/posts";
+import Link from "next/link";
+import Date from "@/components/date";
+import Image from "next/image";
 
-export default function Contact() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Blog({ allPostsData }) {
   return (
-    <BlankLayout column>
+    <BlankLayout>
       <Head>
-        <title>Contact</title>
+        <title>Blog</title>
       </Head>
-      <div></div>
+
+      <div>
+        <h1 className="text-center text-xl">Latest Blog Posts</h1>
+
+        <div className="flex flex-wrap justify-center">
+          {allPostsData.map(({ id, date, title, image }) => (
+            <div
+              key={id}
+              className="group relative m-6 h-80 w-full overflow-hidden rounded-md md:h-52 md:w-80"
+            >
+              <Link href={`/blog/${id}`}>
+                <Image
+                  src={"/images/" + image}
+                  fill
+                  alt=""
+                  className="object-cover duration-100 ease-in group-hover:scale-105"
+                />
+                <div className="relative flex h-full w-full flex-col justify-end bg-black bg-opacity-50 p-6 group-hover:bg-opacity-30 md:p-3">
+                  <span className="mb-3 text-xl font-semibold text-white">
+                    {title}
+                  </span>
+
+                  <span className="text-sm font-semibold text-gray-300">
+                    <Date dateString={date} />
+                  </span>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
     </BlankLayout>
   );
 }
